442c2b64cac42529b961f419b453bee6b6db9c04
Tonmoy Sardar <tonmay10fcb@gmail.com>
2023-09-16 14:27:55 +0530

    oauth login done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,6 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 import Search from "./Search";
-import user_img from "../media/user_img.svg";
 import Cardlist from "./Cardlist";
 import Charts from "./Charts";
 import DoughnutChart from "./DoughnutChart";
@@ -10,16 +9,24 @@ import whatsapp from "../media/whatsapp.svg";
 import insta from "../media/instagram.svg";
 import mail from "../media/Mail.svg";
 import twitter from "../media/twitter.svg";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Dashboard = () => {
+  const { user,loginWithRedirect,logout } = useAuth0();
+  console.log(user)
   return (
     <div className="px-8 md:px-0 h-full">
       <div className="w-full flex justify-between items-center">
         <p className="text-gray-900 font-bold text-lg md:text-2xl">Dashboard</p>
-        <div className="flex w-4/5 md:w-2/3 justify-end items-center">
+        <div className="flex w-full md:w-2/3 justify-end items-center">
           <Search />
-          <FontAwesomeIcon icon={faBell} className="mx-2" />
-          <img src={user_img} alt="user" className="ml-2 md:ml-4" />
+          <FontAwesomeIcon icon={faBell} className="mx-4" />
+         {user?<div className="flex items-center"> <img src={user.picture} alt="user" className="border-2 border-gray-800 rounded-full w-8 h-8 mr-2 md:mr-4" /><button className="bg-red-500 hover:bg-red-600 text-white font-semibold px-2 rounded-full" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+            LogOut
+          </button></div>:
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-2 rounded-full" onClick={() => loginWithRedirect()}>
+            Login
+          </button>}
         </div>
       </div>
       <Cardlist />
@@ -34,13 +41,17 @@ const Dashboard = () => {
           <div className="border-[1px] border-gray-300 rounded-[15px] shadow-md shadow-gray-200 p-4">
             <div className="w-full h-full">
               <p className="text-gray-900 font-bold text-lg md:text-2xl text-left p-2">
-                John Doe
+                {user?user.name:'Please Log In'}
               </p>
               <ul className="flex flex-wrap justify-between items-center py-2">
                 <li className="w-full md:w-1/2 p-2">
                   {" "}
                   <div className="flex">
-                    <img src={whatsapp} alt="whatsapp" className="w-4 h-4 rounded-full w-max h-max bg-green-300 bg-opacity-60 p-1 text-gray-100" />
+                    <img
+                      src={whatsapp}
+                      alt="whatsapp"
+                      className="w-4 h-4 rounded-full w-max h-max bg-green-300 bg-opacity-60 p-1 text-gray-100"
+                    />
                     <p className="font-light text-sm text-gray-700 underline mx-2">
                       +91 29938 81234
                     </p>
@@ -49,27 +60,39 @@ const Dashboard = () => {
                 <li className="w-full md:w-1/2 p-2">
                   {" "}
                   <div className="flex">
-                    <img src={insta} alt="insta" className="w-4 h-4 rounded-full w-max h-max bg-red-300 bg-opacity-60 p-1 text-gray-100" />
+                    <img
+                      src={insta}
+                      alt="insta"
+                      className="w-4 h-4 rounded-full w-max h-max bg-red-300 bg-opacity-60 p-1 text-gray-100"
+                    />
                     <p className="font-light text-sm text-gray-700 underline mx-2">
-                      john_official
+                      {user?user.nickname:'Unknown'}
                     </p>
                   </div>
                 </li>
                 <li className="w-full md:w-1/2 p-2">
                   {" "}
                   <div className="flex">
-                    <img src={mail} alt="mail" className="w-4 h-4 rounded-full w-max h-max bg-indigo-300 bg-opacity-60 p-1 text-gray-100" />
+                    <img
+                      src={mail}
+                      alt="mail"
+                      className="w-4 h-4 rounded-full w-max h-max bg-indigo-300 bg-opacity-60 p-1 text-gray-100"
+                    />
                     <p className="font-light text-sm text-gray-700 underline mx-2">
-                      john@xyz.com
+                    {user?user.nickname:'Unknown'}@xyz.com
                     </p>
                   </div>
                 </li>
                 <li className="w-full md:w-1/2 p-2">
                   {" "}
                   <div className="flex">
-                    <img src={twitter} alt="twitter" className="w-4 h-4 rounded-full w-max h-max bg-red-300 bg-opacity-60 p-1 text-gray-100" />
+                    <img
+                      src={twitter}
+                      alt="twitter"
+                      className="w-4 h-4 rounded-full w-max h-max bg-red-300 bg-opacity-60 p-1 text-gray-100"
+                    />
                     <p className="font-light text-sm text-gray-700 underline mx-2">
-                     john_official
+                    {user?user.nickname:'Unknown'}
                     </p>
                   </div>
                 </li>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-1d8eb6nukq2zyini.us.auth0.com"
+    clientId="C8eopiuCxzYCoDdTgocaWTFpqfSRxY70"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
